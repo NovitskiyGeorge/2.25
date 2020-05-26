3949c2a5c0965f958a2eab6a50af32b0bd267fe8
George <novitskiy.gs@gmail.com>
2020-05-26 08:06:09 +0300

    task 2.25

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+let btn = document.getElementById('start'),
+    list = document.getElementsByClassName('result-table')[0],
+    value = list.getElementsByClassName('value'),
+    input = document.getElementsByClassName('expenses-item'),
+    chappbtn = document.getElementsByTagName('button')[0],
+    opappbtn = document.getElementsByTagName('button')[1],
+    countbtn = document.getElementsByTagName('button')[2],
+    optionalExp = document.querySelectorAll('.optionalexpenses-item'),
+    chIncome = document.querySelector('.choose-income'),
+    checkbox = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('.month-value'),
+    day = document.querySelector('.day-value');
+
+
+
+console.log(list);
+console.log(value);
+console.log(input);
+console.log(chappbtn);
+console.log(opappbtn);
+console.log(countbtn);
+console.log(optionalExp);
+console.log(chIncome);
+console.log(checkbox);
+console.log(sum);
+console.log(percent);
+console.log(year);
+console.log(month);
+console.log(day);
+
